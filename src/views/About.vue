e66e70c64c6478d5c6cf380b9fa5225cb6aa427f
jinlixue <798284910@qq.com>
2021-05-02 17:19:04 +0800

    初始化项目

---
+++ src/views/About.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <img src="/static/logo.png" alt="" srcset="">
+  </div>
+</template>
+<script>
+// import Vue from 'vue'
+
+import { Toast } from 'vant'
+// Vue.use(Toast);
+export default {
+  mounted() {
+    console.log(this.$route.query)
+    Toast.loading({
+      message: '加载中...',
+      forbidClick: true
+    })
+  }
+}
+</script>
+<style lang="less" scoped>
+  .about {
+    width: 200px;
+    h1 {
+      font-size: 16px;
+    }
+  }
+</style>
